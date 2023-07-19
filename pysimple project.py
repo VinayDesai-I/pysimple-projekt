f8bbfb4833df3e94a91e146b53a53178bc0be338
vinay <136108213+VinayDesai-I@users.noreply.github.com>
2023-07-19 19:52:19 +0530

    Add files via upload

--- pysimple project.py
+++ pysimple project.py
@@ -3,14 +3,19 @@ import PySimpleGUI as gui
 # BASIC WORKING?  ILL INPUT (PRODUCT) ID AND QUANTIT
 import tabulate as t
 def billing_table():
-    data = [['banana','20','5'],['bottles','200','50'],['blah','10000','6']]
-    a = "y"
+    data = [['banana',20,5],['bottles',200,4],['blah',10000,6]]
     table = []
+    head=['ID','Product Name','Quantity','price']
+    a = 'y'
     while a == "y" or a == "Y":
         i = int(input("Enter the Product ID:"))
         q = int(input("Enter the Quantity:")) 
         a = input("Do you want to continue? Press y to continue and n to stop")
-        table = table.append(data[i])
-        table = table.append(q)
-    print(t(table))
-print(billing_table())
+        table.append([i,data[i][0],q,data[i][2]])
+    print(t.tabulate(table,head))
+billing_table()
+#.append([data[i][0]]).append([q]).append([data[i][2]])
+
+# GUI
+def gui_table():
+    headings
